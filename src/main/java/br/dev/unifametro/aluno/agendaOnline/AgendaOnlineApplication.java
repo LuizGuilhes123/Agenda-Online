94adbc9ce6d1298c6adbb1fec97c033dbd1a4c89
LuizGuilhes123 <luizguilhermeinformat@gmail.com>
2024-12-13 08:41:44 -0300

    first commit #0

---
+++ src/main/java/br/dev/unifametro/aluno/agendaOnline/AgendaOnlineApplication.java
@@ -0,0 +1,13 @@
+package br.dev.unifametro.aluno.agendaOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/unifametro/aluno/agendaOnline/AgendaOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.unifametro.aluno.agendaOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
